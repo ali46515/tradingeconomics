fcd51b8c18b77adf7080cbec8ecc91d29e74754b
tradingeconomics <aliarshad.9474@gmail.com>
2026-03-15 16:39:17 +0500

    Added Tailwindcss and sidebar to the project

--- country-comparison-dashboard/vite.config.js
+++ country-comparison-dashboard/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+import path from "path";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "::",
+    port: 8080,
+    
+    hmr: {
+      overlay: false,
+    },
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
